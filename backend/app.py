41255f540cda48645dc9b80b24b2cc33107f39f1
Kiran Nalwade <nalwadekiran2007@gmail.com>
2026-08-28 16:45:41 +0530

    Fix home page navigation

--- backend/app.py
+++ backend/app.py
@@ -82,6 +82,10 @@ def register():
     except Exception as err:
         return f"Registration Failed: {err}"
 
+    @app.route("/login")
+    def login_page():
+     return render_template("login.html")
+
 
 # =====================================================
 # LOGIN
