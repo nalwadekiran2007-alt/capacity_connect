0a3b6d67ae51f036b9c18e82f9b8b3ca7303c93a
Kiran Nalwade <nalwadekiran2007@gmail.com>
2026-08-29 11:06:53 +0530

    Fix register page rout

--- backend/app.py
+++ backend/app.py
@@ -52,6 +52,9 @@ def test_db():
 # =====================================================
 # REGISTER
 # =====================================================
+@app.route("/register")
+def register_page():
+    return render_template("register.html")
 
 @app.route("/register", methods=["POST"])
 def register():
